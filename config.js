32c78bb5a7c2e648a1e52aeb7af78be64c5f0d7c
thimashaudam <147721114+thimashaudam@users.noreply.github.com>
2023-10-12 05:36:06 -0700

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94778864288', 'me', true],
+  ['94778864288', 'THIMASHA UDAM', true], 
   [''] 
 ] //Number of owners
 
